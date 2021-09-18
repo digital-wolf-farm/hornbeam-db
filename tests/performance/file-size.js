a4f2264b4755cda1677b59939957e13bee0cd9f6
Digital Wolf Farm <digital.wolf.farm@gmail.com>
2021-09-18 15:01:52 +0200

    Performance testing added to verify concept of stat file before reading data

--- tests/performance/file-size.js
+++ tests/performance/file-size.js
@@ -1,9 +1,12 @@
-import { writeFileSync, readFileSync, unlinkSync } from 'fs';
+import { writeFileSync, readFileSync, statSync, unlinkSync } from 'fs';
 import { performance } from 'perf_hooks';
 
 const startWriting = 'startWriting';
 const endWriting = 'endWriting';
 const writingTask = 'writingTask';
+const startStat = 'startStat';
+const endStat = 'endStat';
+const statTask = 'statTask';
 const startReading = 'startReading';
 const endReading = 'endReading';
 const readingTask = 'readingTask';
@@ -16,7 +19,7 @@ const exampleObject = {
     key2: 'test2'.repeat(1024)
 }
 
-const data = new Array(1000).fill({ ...exampleObject });
+const data = new Array(100000).fill({ ...exampleObject });
 
 console.log('Data size in kB: ', Buffer.byteLength(JSON.stringify(data)) / 1024);
 
@@ -28,6 +31,10 @@ function readFile() {
     readFileSync(filePath, { encoding: 'utf-8' });
 }
 
+function statFile() {
+    statSync(filePath);
+}
+
 performance.mark(startWriting);
 
 writeFile();
@@ -35,6 +42,14 @@ writeFile();
 performance.mark(endWriting);
 performance.measure(writingTask, startWriting, endWriting);
 console.log('Writing file time in ms: ', performance.nodeTiming.duration);
+
+performance.mark(startStat);
+
+statFile();
+
+performance.mark(endStat);
+performance.measure(startStat, startStat, endStat);
+console.log('Stat file time in ms: ', performance.nodeTiming.duration);
 
 performance.mark(startReading);
 
